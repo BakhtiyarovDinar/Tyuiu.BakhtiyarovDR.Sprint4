271b3b4ec7b4a2bce19c5e67aecc718ead5bbe94
Tyuiu.BakhtiyarovDR@outlook.com <dinar_bahtiyarov2005@mail.ru>
2023-11-12 17:28:08 +0500

    Task6

---
+++ Tyuiu.BakhtiyarovDR.Sprint4.Task6.V4.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.BakhtiyarovDR.Sprint4.Task6.V4.Lib
+{
+    public class DataService : ISprint4Task6V4
+    {
+        public string[] Calculate(string[] array)
+        {
+            string[] mas = Array.FindAll(array, colors => colors.Length > 5);
+            return mas;
+        }
+    }
+}
---
+++ Tyuiu.BakhtiyarovDR.Sprint4.Task6.V4.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BakhtiyarovDR.Sprint4.Task6.V4.Lib;
+
+namespace Tyuiu.BakhtiyarovDR.Sprint4.Task6.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var colors = new string[] { "Белый", "Черный", "Зеленый", "Синий", "Красный", "Желтый", "Фиолетовый" };
+            string[] res = ds.Calculate(colors);
+            string[] wait = { "Черный", "Зеленый", "Красный", "Желтый", "Фиолетовый" };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
