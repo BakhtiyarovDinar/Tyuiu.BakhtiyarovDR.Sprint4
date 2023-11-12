901e8aaf3a79760b2dcf81e6a36e6ec342997c55
Tyuiu.BakhtiyarovDR@outlook.com <dinar_bahtiyarov2005@mail.ru>
2023-11-12 17:09:14 +0500

    Revert "Delete Tyuiu.BakhtiyarovDR.Sprint4.Task4.V10 directory"
    This reverts commit e66cccbef3381f32f5cbae9ee0f128756d9b16f0.

---
+++ Tyuiu.BakhtiyarovDR.Sprint4.Task4.V10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.BakhtiyarovDR.Sprint4.Task4.V10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
